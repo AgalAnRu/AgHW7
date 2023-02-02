f342d450aebab498a14a6ebe0eb022e536b7b454
Андрей Сергеевич Агальцов <a.s.agaltsov@yandex.ru>
2023-02-02 18:25:06 +0300

    add Table

---
+++ AgHW7/Clases/Table.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AgHW7.Clases
+{
+    internal class Table
+    {
+        internal int Width;
+        internal int Height;
+        internal int Angle;
+        internal Point TopLeft;
+        private static int WidthMax = 3;
+        private static int HeightMax = 3;
+        private static int AriaMax = 6;
+        private static Random Rnd = new Random();
+
+        internal Table()
+        {
+
+        }
+    }
+}
