643cfe4dcdceb1c0741a3df2892f0b6b43d9ecda
Андрей Сергеевич Агальцов <a.s.agaltsov@yandex.ru>
2023-02-02 19:27:33 +0300

    2

--- AgHW7/Clases/Table.cs
+++ AgHW7/Clases/Table.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Drawing;
+
 
 
 namespace AgHW7.Clases
@@ -22,7 +20,7 @@ namespace AgHW7.Clases
 
         internal Table()
         {
-
+            TopLeft++;
         }
     }
 }
